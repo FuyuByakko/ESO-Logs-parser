8e3b90d785e1978c528ba2897e7f5bec83061c9f
Igor Michailov <igor.m.byak@gmail.com>
2019-10-27 22:57:52 +0900

    completed graphQL DB query handlers

--- models/resolvers.js
+++ models/resolvers.js
@@ -1,24 +1,81 @@
+function updateData(originalObj, updateInfo) {
+  for (const [key, value] of Object.entries(updateInfo)) {
+    originalObj[key] = value;
+  }
+  return originalObj;
+}
+
 module.exports = (knex) => {
   return {
-    Logs: async () => await knex("logs").select(),
-    ServerLogs: async (req) =>
-      await knex("logs")
+    /*
+    QUERY METHODS
+    */
+    Logs: () => knex("logs").select(),
+    ServerLogs: (req) =>
+      knex("logs")
         .where(`server`, `like`, `%${req.server}%`)
         .select(),
-    Characters: async () => await knex("players").select(),
-    CharactersByType: async (req) =>
-      await knex("players")
+    Characters: () => knex("players").select(),
+    CharactersByType: (req) =>
+      knex("players")
         .where(`type`, `=`, `${req.type}`)
         .select(),
-    Character: async (req) => {
+    Character: (req) => {
       if (req.character_id)
-        return await knex("players")
+        return knex("players")
           .where(`character_id`, `like`, `${req.character_id}`)
           .select();
       if (req.character_name)
-        return await knex("players")
+        return knex("players")
           .where(`character_name`, `like`, `${req.character_name}`)
           .select();
     },
+
+    /*
+    MUTATION METHODS
+    */
+
+    //methods for Log manipulation
+    AddLog: (input) =>
+      knex("logs")
+        .insert(input.info)
+        .then(() => {
+          return knex("logs")
+            .where(`log_nr`, `=`, `${input.info.log_nr}`)
+            .select();
+        })
+        .then((value) => value.pop()),
+    EditLog: (input) =>
+      knex("logs")
+        .where(`log_nr`, `=`, `${input.log_nr}`)
+        .select()
+        .then((log) => updateData(log.pop(), input.info)),
+    RemoveLog: (input) =>
+      knex("logs")
+        .where(`log_nr`, `=`, `${input.log_nr}`)
+        .del()
+        .then((res) => `Deleted ${res} row('s)`),
+
+    //methods to deal with character mutations
+    AddCharacter: (input) =>
+      knex("players")
+        .insert(input.info)
+        .then(() => {
+          return knex("players")
+            .where(`character_id`, `=`, `${input.info.character_id}`)
+            .select();
+        })
+        .then((value) => value.pop()),
+    EditCharacter: (input) =>
+      knex("players")
+        .where(`character_id`, `like`, `%${input.character_id}%`)
+        .orWhere(`character_name`, `like`, `%${input.character_name}%`)
+        .select()
+        .then((character) => updateData(character.pop(), input.info)),
+    RemoveCharacter: (input) =>
+      knex("players")
+        .where(`log_nr`, `=`, `${input.log_nr}`)
+        .del()
+        .then((res) => `Deleted ${res} row('s)`),
   };
 };
